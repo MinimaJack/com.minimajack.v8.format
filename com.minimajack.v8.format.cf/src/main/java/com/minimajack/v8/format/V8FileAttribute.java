5c0b2244921c9161b98bc387117ea8b49367a16a
53316dc64382ec6b900007c5 <minimajack@gmail.com>
2016-01-19 09:13:55 +0300

    Some clean ups

--- com.minimajack.v8.format.cf/src/main/java/com/minimajack/v8/format/V8FileAttribute.java
+++ com.minimajack.v8.format.cf/src/main/java/com/minimajack/v8/format/V8FileAttribute.java
@@ -2,117 +2,105 @@ package com.minimajack.v8.format;
 
 import java.io.DataOutput;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.util.Date;
 
 import com.minimajack.v8.utils.DateUtils;
 
-public class V8FileAttribute
-    extends BlockHeader
-{
+public class V8FileAttribute extends BlockHeader {
 
-    public static final int ATTIBUTES_SIZE = 24; // 2x8 + 1x4 + 1x4
+	public static final int ATTIBUTES_SIZE = 24; // 2x8 + 1x4 + 1x4
 
-    public static final Charset DEFAULT_CHARSET = Charset.forName( "UTF-16LE" );
+	public static final int MINIMUM_PAYLOAD_SIZE = 32; // 2x8 + 1x4 + 1x4
 
-    private long creationDate;
+	public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-16LE");
 
-    private long modifyDate;
+	private long creationDate;
 
-    private String name;
+	private long modifyDate;
 
-    private int attributes;
+	private String name;
 
-    public V8FileAttribute()
-    {
-        super();
-    }
+	private int attributes;
 
-    public V8FileAttribute( ByteBuffer buffer, int position )
-    {
-        super( buffer, position );
-    }
+	public V8FileAttribute() {
+		super();
+	}
 
-    @Override
-    public void read()
-        throws IOException
-    {
-        this.readHeader();
-        this.creationDate = getBuffer().getLong();
-        this.modifyDate = getBuffer().getLong();
-        getBuffer().getInt(); // reserved
+	public V8FileAttribute(ByteBuffer buffer, int position) {
+		super(buffer, position);
+	}
 
-        byte[] stringArray = new byte[this.getDocSize() - ATTIBUTES_SIZE];
-        this.getBuffer().get( stringArray );
-        V8FileAttribute.this.name = new String( stringArray, DEFAULT_CHARSET );
-        this.attributes = getBuffer().getInt();
-    }
+	@Override
+	public void read() throws IOException {
+		this.readHeader();
+		this.creationDate = getBuffer().getLong();
+		this.modifyDate = getBuffer().getLong();
+		getBuffer().getInt(); // reserved
 
-    @Override
-    public void write( DataOutput buffer )
-        throws IOException
-    {
-        int payloadSize = getPayloadSize();
+		byte[] stringArray = new byte[this.getDocSize() - ATTIBUTES_SIZE];
+		this.getBuffer().get(stringArray);
+		V8FileAttribute.this.name = new String(stringArray, DEFAULT_CHARSET);
+		this.attributes = getBuffer().getInt();
+	}
 
-        this.setDocSize( payloadSize );
+	@Override
+	public void write(DataOutput buffer) throws IOException {
+		int payloadSize = getPayloadSize();
 
-        buffer.writeLong( creationDate );
-        buffer.writeLong( modifyDate );
-        buffer.writeInt( 0 );
-        buffer.write( name.getBytes( DEFAULT_CHARSET ) );
-        buffer.writeInt( attributes );
-        buffer.write( new byte[this.getBlockSize() - payloadSize] );
-    }
+		this.setDocSize(payloadSize);
 
-    public int getPayloadSize()
-        throws UnsupportedEncodingException
-    {
-        int payloadSize = ATTIBUTES_SIZE;
-        byte[] names = name.getBytes( DEFAULT_CHARSET );
-        payloadSize += names.length;
-        return payloadSize;
-    }
+		buffer.writeLong(creationDate);
+		buffer.writeLong(modifyDate);
+		buffer.writeInt(0);
+		buffer.write(name.getBytes(DEFAULT_CHARSET));
+		buffer.writeInt(attributes);
+		buffer.write(new byte[this.getBlockSize() - payloadSize]);
+	}
 
-    public Date getCreationDate()
-    {
-        return DateUtils.fromv8Time( this.creationDate );
-    }
+	@Override
+	public void setDocSize(int docSize) {
+		super.setDocSize(Math.max(MINIMUM_PAYLOAD_SIZE, docSize));
+	}
 
-    public void setCreationDate( Date creationDate )
-    {
-        this.creationDate = DateUtils.tov8Time( creationDate );
-    }
+	public int getPayloadSize() {
+		int payloadSize = ATTIBUTES_SIZE;
+		byte[] names = name.getBytes(DEFAULT_CHARSET);
+		payloadSize += names.length;
+		return payloadSize;
+	}
 
-    public Date getModifyDate()
-    {
-        return DateUtils.fromv8Time( this.modifyDate );
-    }
+	public Date getCreationDate() {
+		return DateUtils.fromv8Time(this.creationDate);
+	}
 
-    public void setModifyDate( Date modifyDate )
-    {
-        this.modifyDate = DateUtils.tov8Time( modifyDate );
-    }
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = DateUtils.tov8Time(creationDate);
+	}
 
-    public int getAttributes()
-    {
-        return attributes;
-    }
+	public Date getModifyDate() {
+		return DateUtils.fromv8Time(this.modifyDate);
+	}
 
-    public void setAttributes( int attributes )
-    {
-        this.attributes = attributes;
-    }
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = DateUtils.tov8Time(modifyDate);
+	}
 
-    public String getName()
-    {
-        return name;
-    }
+	public int getAttributes() {
+		return attributes;
+	}
 
-    public void setName( String name )
-    {
-        this.name = name;
-    }
+	public void setAttributes(int attributes) {
+		this.attributes = attributes;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
